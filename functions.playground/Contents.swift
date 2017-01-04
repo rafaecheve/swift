6364acaf65b4813a96d52ddfb564bc0caff225d4
Rafael Echeverria <rafael.echverria@gmail.com>
2017-01-04 15:59:29 -0600

    Add files via upload

---
+++ functions.playground/Contents.swift
@@ -0,0 +1,31 @@
+
+
+func sayHelloWorld() {
+    
+    print("Hello, world!")
+
+}
+
+func sayHelloTo(name: String) {
+    
+    print("Hello, \(name)!")
+
+}
+
+func say(_ greeting: String, to: String) {
+
+    print("\(greeting), \(to)!")
+    
+}
+
+say("GoodBye", to: "Hollywood")
+
+func add(_ a: Int, _ b: Int) -> Int {
+
+    return a + b
+
+}
+
+add(4,5)
+
+
